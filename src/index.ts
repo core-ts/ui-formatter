71fd3b74609f0d0e6c76c5c9c0ecc0774c7410bb
Duc Nguyen <minhduc140583@gmail.com>
2025-08-10 23:28:19 +0700

    Add formatNumber and formatPhone

--- src/index.ts
+++ src/index.ts
@@ -144,3 +144,119 @@ function pad3(n: number): string {
   }
   return n < 10 ? "00" + n : "0" + n.toString()
 }
+
+export function formatNumber(v: number, scale?: number, d?: string | null, g?: string): any {
+  if (!v) {
+    return ""
+  }
+  if (!d && !g) {
+    g = ","
+    d = "."
+  } else if (!g) {
+    g = d === "," ? "." : ","
+  }
+  const s = scale === 0 || scale ? v.toFixed(scale) : v.toString()
+  const x = s.split(".", 2)
+  const y = x[0]
+  const arr: string[] = []
+  const len = y.length - 1
+  for (let k = 0; k < len; k++) {
+    arr.push(y[len - k])
+    if ((k + 1) % 3 === 0) {
+      arr.push(g)
+    }
+  }
+  arr.push(y[0])
+  if (x.length === 1) {
+    return arr.reverse().join("")
+  } else {
+    return arr.reverse().join("") + d + x[1]
+  }
+}
+
+// tslint:disable-next-line:class-name
+export class formatter {
+  // private static _preg = / |\+|\-|\.|\(|\)/g;
+  static phone = / |\-|\.|\(|\)/g
+  static usPhone = /(\d{3})(\d{3})(\d{4})/
+  static removePhoneFormat(phone: string): string {
+    if (phone) {
+      return phone.replace(formatter.phone, "")
+    } else {
+      return phone
+    }
+  }
+  static removeFaxFormat(fax: string): string {
+    if (fax) {
+      return fax.replace(formatter.phone, "")
+    } else {
+      return fax
+    }
+  }
+  static formatPhone(phone?: string | null): string {
+    if (!phone) {
+      return ""
+    }
+    // reformat phone number
+    // 555 123-4567 or (+1) 555 123-4567
+    let s = phone
+    const x = formatter.removePhoneFormat(phone)
+    if (x.length === 10) {
+      const USNumber = x.match(formatter.usPhone)
+      if (USNumber != null) {
+        s = `${USNumber[1]} ${USNumber[2]}-${USNumber[3]}`
+      }
+    } else if (x.length <= 3 && x.length > 0) {
+      s = x
+    } else if (x.length > 3 && x.length < 7) {
+      s = `${x.substring(0, 3)} ${x.substring(3, x.length)}`
+    } else if (x.length >= 7 && x.length < 10) {
+      s = `${x.substring(0, 3)} ${x.substring(3, 6)}-${x.substring(6, x.length)}`
+    } else if (x.length >= 11) {
+      const l = x.length
+      s = `${x.substring(0, l - 7)} ${x.substring(l - 7, l - 4)}-${x.substring(l - 4, l)}`
+      // formatedPhone = `(+${phoneNumber.charAt(0)}) ${phoneNumber.substring(0, 3)} ${phoneNumber.substring(3, 6)}-${phoneNumber.substring(6, phoneNumber.length - 1)}`;
+    }
+    return s
+  }
+  static formatFax(fax?: string | null): string {
+    if (!fax) {
+      return ""
+    }
+    // reformat phone number
+    // 035-456745 or 02-1234567
+    let s = fax
+    const x = formatter.removePhoneFormat(fax)
+    const l = x.length
+    if (l <= 6) {
+      s = x
+    } else {
+      if (x.substring(0, 2) !== "02") {
+        if (l <= 9) {
+          s = `${x.substring(0, l - 6)}-${x.substring(l - 6, l)}`
+        } else {
+          s = `${x.substring(0, l - 9)}-${x.substring(l - 9, l - 6)}-${x.substring(l - 6, l)}`
+        }
+      } else {
+        if (l <= 9) {
+          s = `${x.substring(0, l - 7)}-${x.substring(l - 7, l)}`
+        } else {
+          s = `${x.substring(0, l - 9)}-${x.substring(l - 9, l - 7)}-${x.substring(l - 7, l)}`
+        }
+      }
+    }
+    return s
+  }
+}
+export function removePhoneFormat(phone: string): string {
+  return formatter.removePhoneFormat(phone)
+}
+export function removeFaxFormat(fax: string): string {
+  return formatter.removeFaxFormat(fax)
+}
+export function formatPhone(phone?: string | null): string {
+  return formatter.formatPhone(phone)
+}
+export function formatFax(fax?: string | null): string {
+  return formatter.formatFax(fax)
+}
